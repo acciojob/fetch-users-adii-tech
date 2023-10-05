e94bb7a19c967a32b0d5c3db448212110b074fbd
Aditya Kadam <79622885+adii-tech@users.noreply.github.com>
2023-10-05 14:40:29 +0000

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
 
 import React from "react";
 import './../styles/App.css';
+import axios from "axios";
+import 'regenerator-runtime/runtime';
+import { useEffect,useState } from "react";
 
 const App = () => {
+const [data,setData]=useState([]);
+let responce=[];
+
+  useEffect(()=>{
+  const fetchdata= async()=>{
+       responce=await axios.get("https://reqres.in/api/users");
+       console.log(responce.data.data);
+       
+  }
+  fetchdata();
+},[])
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>
+          <h2>Blue Whales</h2>
+          <button classname="btn" onClick={()=>setData(responce.data.data)}>Get User List</button>
+        </div>
+        <nav className="navbar">
+          <ul className="nav">
+            <li>First Name</li>
+            <li>Last Name</li>
+            <li>Email</li>
+            <li>Avatar</li>
+          </ul>
+        </nav>
+        <ul className="mainList"> {
+      data && 
+        data.map(ele=>(
+          <li><div className="container"><ul className="list">
+            <li>{ele.first_name}</li>
+            <li>{ele.last_name}</li>
+            <li>{ele.email}</li>
+            <li><img src={ele.avatar}></img></li>
+            </ul>
+            </div>
+            </li>
+            
+            ) )
+
+     }</ul>
     </div>
   )
 }
